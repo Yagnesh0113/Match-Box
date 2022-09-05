6b12364f0cf701b58b8c701fd73780b46e110ebd
Yagnesh0113 <109940839+Yagnesh0113@users.noreply.github.com>
2022-09-05 11:51:27 +0530

    edit changes

--- Account/models.py
+++ Account/models.py
@@ -87,9 +87,29 @@ class ProfessionReview(models.Model):
     )
     Review_Date=models.DateField(default=date.today())
     Review_Time=models.TimeField(default=datetime.now())
+    Like=models.ManyToManyField(to=UserProfile,null=True, blank=True, related_name='liked')
 
     def __str__(self):
       return self.Review
+
+    @property
+    def num_likes(self):
+        return self.Like.all().count()
+
+
+LIKE_CHOISE = (
+    ('Like', 'Like'),
+    ('Unlike', 'Unlike'),
+)
+
+
+class Review_Like(models.Model):
+    user_profile = models.ForeignKey(to=UserProfile, on_delete=models.CASCADE)
+    profession = models.ForeignKey(to=Profession, on_delete=models.CASCADE)
+    value = models.CharField(choices=LIKE_CHOISE, default='Like', max_length=10)
+
+    def __str__(self):
+        return str(self.profession)
 
 class ProfessionReview_Reply(models.Model):
     Review= models.ForeignKey(to=ProfessionReview,on_delete=models.CASCADE)
