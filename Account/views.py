5d2db6acf55d173a8536b6004d26b71306d10c69
Ankush nag <54714148+Ankushnag@users.noreply.github.com>
2022-09-13 16:37:05 +0530

    changes

--- Account/views.py
+++ Account/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.http import HttpResponse
 from django.contrib import messages
-import re
+import re   
 
 
 
