2ce3749d682c5254752584a324aa0bddb8331027
Ankit FSD <ankitpy.snable@gmail.com>
2022-10-03 17:15:21 +0530

    community
    community

---
+++ community_profession/static/community_profession/js/news-comment.js
@@ -0,0 +1,6 @@
+/* Start edit edit comment */
+function data(id,val1){
+   document.getElementById('Editcomment').action ='/edit_news_comment/'+id;
+   document.getElementById("demo1").value = val1;
+   }
+/* End edit edit comment */
---
+++ community_profession/static/community_profession/js/news.js
@@ -0,0 +1,6 @@
+/* Start the report-news script */
+function Reportnews(id){
+      document.getElementById('report-news').action ='';
+      document.getElementById("demo1").value = id;
+   }
+/* End the report-news script */
